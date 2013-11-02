2816b18b6ab240aa3991dd70d80569a9a3542e1a
Dipen Routaray <try4dipen@gmail.com>
2013-11-03 00:04:29 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '332b78c63fc6adf8da2fe4cb9c34639e9bb6fe6ea12938b6e80bb462bf459ef378b2f87b2677de43cd1b9ef731fba0a187a960102798d68c5aee04be371d88bc'
